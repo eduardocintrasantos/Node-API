302065f2932b56a2b55e73ae3000c87c82128617
Eduardo Cintra dos Santos <eduardocintradossantos@gmail.com>
2023-03-22 20:19:31 -0300

    Delete

--- app.js
+++ app.js
@@ -99,6 +99,20 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     })
 })
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+
+    if(req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'Fail',
+            message: 'Invalid ID'
+        })
+    }
+
+    res.status(204).json({
+        status: 'Sucesso',
+        data: null
+    })
+})
 const port = 3000;
 app.listen(port, () => {
     console.log(`App rodando na porta ${port}`)
