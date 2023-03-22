57689de506bc6fec52a743c1319e232e102010a0
Eduardo Cintra dos Santos <eduardocintradossantos@gmail.com>
2023-03-22 15:36:15 -0300

    Start projeto, Get, Post

---
+++ app.js
@@ -0,0 +1,86 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+/*  EXEMPLOS
+
+app.get('/', (req, res) => {
+    res.status(200).send('teste');
+});
+
+app.post('/', (req, res) => {
+    res.status(200)
+    .send('Post...');
+});
+
+app.get('/produtos', (req, res) => {
+    res
+    .status(200)
+    .json({
+        Produto: 'Caneta', 
+        Cor: 'Azul'
+    });
+});
+*/
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200)
+    .json({
+        status: 'Sucesso',
+        results: tours.length,
+        data: {
+            tours
+        }
+    });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params); // req.params, pega todos os parametros passados na URL, dois pontos : define que é um parametro
+                             // caso o parametro tenha um ponto de interrogação na frente, isso define ele como opcional :x?
+
+    const id = req.params.id * 1; // Multiplicar um numero string, transforma ele para o tipo numerico automaticamente 
+    const tour = tours.find(elemento => elemento.id === id);
+
+    //if(id > tours.length) {
+    if(!tour) {
+        return res.status(404)
+                  .json({
+                    status: 'Fail',
+                    message: 'Id invalido'
+                  });
+    };
+
+    res.status(200)
+    .json({
+        status: 'Sucesso',
+        data: {
+            tour
+        }
+    });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+    //console.log(req.body);
+
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body)
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'Success',
+            data: {
+                tour: newTour
+            }
+        });
+    })
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App rodando na porta ${port}`)
+});
