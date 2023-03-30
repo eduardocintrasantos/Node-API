a2de8024b24b396d9c14190984ac4f0c24d34a59
Eduardo Cintra dos Santos <eduardocintradossantos@gmail.com>
2023-03-30 15:06:12 -0300

    Middleware verificando dados para inserir um novo tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,12 +4,22 @@ const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-si
 
 exports.checkID = (req, res, next, val) => {
     console.log(`Tour id is ${val}`);
-    
+
     if(req.params.id * 1 > tours.length) {
         return res.status(404).json({
             status: 'Fail',
             message: 'Invalid ID'
         });
+    }
+    next();
+}
+
+exports.checkBody = (req, res, next) => {
+    if(!req.body.name || !req.body.price) {
+        return res.status(404).json({
+            status: 'Fail',
+            message: 'Sem informações'
+        })
     }
     next();
 }
